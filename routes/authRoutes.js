74b36cf7a33cd0cb8ff5a03ed6e1428c42d8b2da
Mohd Rafi Uddin <mohdrafi188@gmail.com>
2018-09-08 22:33:37 +0530

    login and logout route is added.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,4 +10,13 @@ module.exports = app => {
     '/auth/google-drive/callback',
      passport.authenticate('google-drive')
    );
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
  }
