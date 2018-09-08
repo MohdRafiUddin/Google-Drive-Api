0512c2c6924c4856d0e35a17564775765f0bd5f7
Mohd Rafi Uddin <mohdrafi188@gmail.com>
2018-09-08 17:58:23 +0530

    google drive passport authentication added.

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const express = require('express'),
+const express = require('express');
+const passport = require('passport');
+const GoogleDriveStrategy = require('passport-google-drive').Strategy;
 app = express();
+const Keys = require('./config/keys.js');
 
-app.get('/', (req, res) => {
-  res.send('home route');
-});
+passport.use(
+    new GoogleDriveStrategy(
+     {
+       clientID: Keys.DRIVE_CLIENT_ID,
+       clientSecret: Keys.DRIVE_CLIENT_SECRET,
+       callbackURL: '/auth/google-drive/callback'
+      },
+      (accessToken, refreshToken, profile, done) => {
+         console.log(accessToken);
+       }
+     )
+   );
+
+app.get('/auth/google-drive', passport.authenticate('google-drive', {scope : 'https://www.googleapis.com/auth/drive.readonly'}));
+
+app.get('/auth/google-drive/callback', passport.authenticate('google-drive'));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
