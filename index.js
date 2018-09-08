c515159b8864b29b98ac7ba012382d9a42ef56e2
Mohd Rafi Uddin <mohdrafi188@gmail.com>
2018-09-08 22:32:25 +0530

    added passport midlewares for initailize.

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 require('./models/users');
 require('./services/passport');
-app = express();
 const keys = require('./config/keys');
 
 mongoose.connect(keys.MONGOOSE_URI);
+
+app = express();
+
+app.use(
+   cookieSession({
+     maxAge: 30*24*60*60*1000,
+     keys: [keys.COOKIEKEY]
+   })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
