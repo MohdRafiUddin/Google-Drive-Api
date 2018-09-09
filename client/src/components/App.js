ad3b72a9b550ae411911fd62ae87465a1bfa3c16
Mohd Rafi Uddin <mohdrafi188@gmail.com>
2018-09-09 23:54:02 +0530

    using connect redux-react lib to dispatch actions .

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
 import Header from './Header';
 import Landing from './Landing';
 import Dashboard from './Dashboard';
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <Route path exact ="/" component={Landing} />
-        <Route path ="/Dashboard" component={Dashboard} />
-      </div>
-    </BrowserRouter>
-  );
+class App extends Component {
+  componentDidMount() {
+     this.props.fetchUser();
+  }
+  render(){
+   return (
+     <BrowserRouter >
+       <div>
+         <Header />
+         <Route exact path="/" component={Landing} />
+         <Route path="/Dashboard" component={Dashboard} />
+       </div>
+     </BrowserRouter>
+   );
+ }
 }
 
-export default App;
+export default connect(null, actions)(App);
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
- render(){
-  return (
-    <h1> Header </h1>
-  );
- }
+  renderContent(){
+    switch (this.props.auth) {
+      case null:
+        return <li>Loading......</li>;
+      case false:
+        return <li><a href="/auth/google-drive">Login With Google Drive</a></li>;
+      default:
+        return <li><a href="/api/logout">Logout</a></li>;
+    }
+  }
+
+  render(){
+   return (
+     <nav>
+       <div className="nav-wrapper">
+         <a  className="left brand-logo">
+           Drive Api
+         </a>
+         <ul className="right">
+           { this.renderContent() }
+         </ul>
+       </div>
+     </nav>
+   );
+  }
 }
-export default Header;
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+export default connect(mapStateToProps)(Header);
