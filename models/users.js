d795102f5c98c60ae5a623d1d41a2d8c39d65a1b
Mohd Rafi Uddin <mohdrafi188@gmail.com>
2018-09-11 02:34:53 +0530

    to existing user model, a new data field is added.

--- models/users.js
+++ models/users.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const userSchema = new Schema({
-   driveID: String
+   driveID: String,
+   data: JSON
  });
 
 mongoose.model('drive-users', userSchema)
