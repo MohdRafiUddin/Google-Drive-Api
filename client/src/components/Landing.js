f4b1d132189c65a7da2b4e369aabd289993447ce
Mohd Rafi Uddin <mohdrafi188@gmail.com>
2018-09-11 23:33:01 +0530

    add styling to landing page.

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 class Landing extends Component {
  render(){
   return (
-    <h1> Landing </h1>
+    <div className="container-1">
+     <div className="container-1-1">
+        <div className="container-1-1-1"><img alt="Drive logo" src="https://www.google.com/drive/static/images/drive/logo-drive.png" /></div>
+        <div className="container-1-1-2">Google Drive - Built on MERN Stack</div>
+        <div className="container-1-1-3"><a href="/Dashboard">Go to Google Drive</a></div>
+     </div>
+    </div>
   );
  }
 }
