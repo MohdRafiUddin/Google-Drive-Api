59fddf2491033506318065b3fe6c5624f08a5c81
Mohd Rafi Uddin <mohdrafi188@gmail.com>
2018-09-11 19:52:41 +0530

    added fetchdata action to fetch list of files from backend.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
+import { FETCH_DATA } from './types';
 
 export const fetchUser = () => {
    return function(dispatch) {
@@ -7,4 +8,12 @@ export const fetchUser = () => {
       .get('/api/current_user')
       .then(res => dispatch({ type: FETCH_USER, payload: res.data }));
     }
+}
+
+export const fetchData = () => {
+  return function(dispatch) {
+    axios
+    .get('api/current_user')
+    .then(res => dispatch({ type: FETCH_DATA, payload: res.data.data}))
+  }
 }
