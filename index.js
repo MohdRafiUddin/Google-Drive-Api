acf26e87864c36e4e4646f87a328b9de598c4e5f
Mohd Rafi Uddin <mohdrafi188@gmail.com>
2018-09-13 01:54:49 +0530

    added downRoute .

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/downRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
