4ba315aa828b9f824037ee2ce60708b3c68227e4
Mohd Rafi Uddin <mohdrafi188@gmail.com>
2018-09-13 01:52:10 +0530

    download post action added.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 import { FETCH_DATA } from './types';
+import { FETCH_RESULT } from './types';
 
 export const fetchUser = () => {
    return function(dispatch) {
@@ -16,4 +17,11 @@ export const fetchData = () => {
     .get('api/current_user')
     .then(res => dispatch({ type: FETCH_DATA, payload: res.data.data}))
   }
+}
+
+export const download = id => async dispatch => {
+    const data = {"id": id}
+    const res = await axios.post('/api/download', data);
+
+    dispatch({ type: FETCH_RESULT, payload: res.data});
 }
