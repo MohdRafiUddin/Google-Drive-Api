d497dcd958fc0e27e00faa9a3d7f5f3851d75af7
Mohd Rafi Uddin <mohdrafi188@gmail.com>
2018-09-14 01:09:52 +0530

    added download and upload reducers to rootReducer.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import dataReducer from './dataReducer';
+import downReducer from './downReducer';
+import uploadReducer from './uploadReducer';
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  data: dataReducer
+  data: dataReducer,
+  down: downReducer,
+  upload: uploadReducer
 });
 
 export default rootReducer;
