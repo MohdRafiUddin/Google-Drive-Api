718e9dbab357d3a03fc5415117a9d979236ce38c
Mohd Rafi Uddin <mohdrafi188@gmail.com>
2018-09-14 02:57:22 +0530

    typeError fixed.

--- config/keys.js
+++ config/keys.js
@@ -2,8 +2,8 @@
 
 if(process.env. NODE_ENV === 'production') {
   //production mode
-  modules.export = require('./prod');
+  module.exports = require('./prod');
 } else {
   //development mode
-  modules.export = require('./dev');
+  module.exports = require('./dev');
 }
