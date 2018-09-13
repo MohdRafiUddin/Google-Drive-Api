3c56772390a0456eda40d77cd976b17e0a11cb14
Mohd Rafi Uddin <mohdrafi188@gmail.com>
2018-09-13 16:14:44 +0530

    added fetch upload type.

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,4 @@
 export const FETCH_USER = 'fetch_user';
 export const FETCH_DATA = 'fetch_data';
-export const FETCH_RESULT = 'fetch_result';
+export const FETCH_DOWN = 'fetch_down';
+export const FETCH_UPLOAD = 'fetch_upload';
