864ff150367125d6df3e4d2c95fa91a6e83d47d0
Mohd Rafi Uddin <mohdrafi188@gmail.com>
2018-09-13 12:18:00 +0530

    refactor to async await syntax.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,21 +3,15 @@ import { FETCH_USER } from './types';
 import { FETCH_DATA } from './types';
 import { FETCH_RESULT } from './types';
 
-export const fetchUser = () => {
-   return function(dispatch) {
-      axios
-      .get('/api/current_user')
-      .then(res => dispatch({ type: FETCH_USER, payload: res.data }));
+export const fetchUser = () => async dispatch {
+      const res = await axios.get('/api/current_user');
+      dispatch({ type: FETCH_USER, payload: res.data });
     }
-}
 
-export const fetchData = () => {
-  return function(dispatch) {
-    axios
-    .get('api/current_user')
-    .then(res => dispatch({ type: FETCH_DATA, payload: res.data.data}))
+export const fetchData = () => async dispatch {
+    const res = await axios.get('api/current_user');
+    dispatch({ type: FETCH_DATA, payload: res.data.data})
   }
-}
 
 export const download = (id, driveID) => async dispatch => {
     const data = { "id": id, "driveID": driveID }
