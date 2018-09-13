5d4cf4ff91d066e4b4bebe87ed7946e3455e3824
Mohd Rafi Uddin <mohdrafi188@gmail.com>
2018-09-13 11:53:21 +0530

    making http get request to google api for file download.

--- routes/downRoutes.js
+++ routes/downRoutes.js
@@ -1,7 +1,20 @@
+const mongoose = require('mongoose');
+const Keys = require('../config/keys.js');
+const User = mongoose.model('drive-users');
+const axios = require('axios');
+const requireLogin = require('../middlewares/requireLogin');
+
 module.exports = app => {
-  app.post('/api/download', (req, res) => {
-    res.send(req.body.id);
-
-
+  app.post('/api/download', requireLogin, (req, res) => {
+    User.findOne({driveID : req.body.driveID})
+    .then( User =>
+       axios.get("https://www.googleapis.com/drive/v3/files/"+req.body.id+"?alt=media", {
+         headers: {
+           "Authorization": "Bearer "+User.token.accessToken
+         }
+       })
+       .then(response => res.send(response))
+       .catch(err => res.send(err))
+    );
   });
  }
