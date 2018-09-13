9ba5b26c4026e236adbcb1f327c8ce88e19fb976
Mohd Rafi Uddin <mohdrafi188@gmail.com>
2018-09-14 02:43:55 +0530

    adding production config.

--- index.js
+++ index.js
@@ -27,5 +27,15 @@ require('./routes/authRoutes')(app);
 require('./routes/downRoutes')(app);
 require('./routes/uploadRoutes')(app);
 
+if(process.env.NODE_ENV === 'production'){
+  //Express will serve up production assets
+  app.use(express.static('client/build'));
+  //Express will serve up index.html file
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
