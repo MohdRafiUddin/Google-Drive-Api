3aef70a93b934725e66b1da36db777943cc2a24a
Mohd Rafi Uddin <mohdrafi188@gmail.com>
2018-09-13 16:14:18 +0530

    added upload action .

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,21 +1,35 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 import { FETCH_DATA } from './types';
-import { FETCH_RESULT } from './types';
+import { FETCH_DOWN } from './types';
+import { FETCH_UPLOAD } from './types';
 
-export const fetchUser = () => async dispatch {
-      const res = await axios.get('/api/current_user');
+export const fetchUser = () => async dispatch => {
+      const res = await axios.get('api/current_user');
       dispatch({ type: FETCH_USER, payload: res.data });
     }
 
-export const fetchData = () => async dispatch {
+export const fetchData = () => async dispatch => {
     const res = await axios.get('api/current_user');
     dispatch({ type: FETCH_DATA, payload: res.data.data})
   }
 
 export const download = (id, driveID) => async dispatch => {
-    const data = { "id": id, "driveID": driveID }
-    const res = await axios.post('/api/download', data);
+    const data = {
+      "id": id,
+      "driveID": driveID
+    }
+    const res = await axios.post('api/download', data);
+    dispatch({ type: FETCH_DOWN, payload: res.data});
+}
 
-    dispatch({ type: FETCH_RESULT, payload: res.data});
+export const upload = (name, size, type, driveID) => async dispatch => {
+    const data = {
+      "name": name,
+      "size": size,
+      "type": type,
+      "driveID": driveID
+    }
+    const res = await axios.post('api/upload', data);
+    dispatch({ type: FETCH_UPLOAD, payload: res.data})
 }
